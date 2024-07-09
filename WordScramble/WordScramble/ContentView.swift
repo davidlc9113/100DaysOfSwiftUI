86eb5fc8c2050188c799ca13e709cfd36d57f4c7
David <dlc9113@gmail.com>
2024-07-09 12:56:12 +0800

    Add project for day 30

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by David Li on 2024/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var useWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(useWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            useWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by David Li on 2024/7/8.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

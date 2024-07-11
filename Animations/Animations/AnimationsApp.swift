1c600097652bd0989631a233c316022c5d9895e4
David <dlc9113@gmail.com>
2024-07-11 12:39:55 +0800

    Add project for day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by David Li on 2024/7/11.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

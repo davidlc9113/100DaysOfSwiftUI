97d1243c253e7ab91966a0dcb05a8a925a4a0160
David <dlc9113@gmail.com>
2024-07-14 20:36:24 +0800

    Add project for day 36

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by David Li on 2024/7/14.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

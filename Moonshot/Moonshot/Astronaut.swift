7f29b66cd83601a4d0ce0968ead63a4364fb6594
David <dlc9113@gmail.com>
2024-07-17 21:09:19 +0800

    Loading a specific kind of Codable data

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by David Li on 2024/7/17.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

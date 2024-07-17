7e6a1f8068ee8fc1ee6ee46af5befb05144a0bd5
David <dlc9113@gmail.com>
2024-07-17 21:25:54 +0800

    Using generics to load any kind of Codable data

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by David Li on 2024/7/17.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
